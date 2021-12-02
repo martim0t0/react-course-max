37e08ca6d8fd3c40ccb4c2716b090998e5f5d824
Marcin Jaszczuk <martim0t0@gmail.com>
2021-12-02 11:37:00 -0600

    Reset project to scratch, then create MainNavigation component in layout subfolder, create AllMeetup, Favorites, and NewMeetup pages in 'pages' folder.

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function MainNavigation() {
+  return (
+    <header>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All Meetups</Link>
+            </li>
+            <li>
+              <Link to="/new-meetup">Add New Meetup</Link>
+            </li>
+            <li>
+              <Link to="/favorites">My Favorites</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      ;
+    </header>
+  );
+}
